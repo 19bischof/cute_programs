336faa768bed22d7a1f1426ae3a7bea9c5d2c646
bischof <moritzalbbw2@gmail.com>
2021-08-03 10:28:52 +0200

    added Rainbow Colors for balls;fixed index out of range with list_of_ufos; added increasing difficulty

--- Viper/alien_invasion/main.py
+++ Viper/alien_invasion/main.py
@@ -1,5 +1,8 @@
 import pygame
 import random
+#important:
+#ufo[] gives first x then y of the upper left corner of the rect.
+#ball[] gives first x then y of the middle of the sphere
 pygame.init()
 screen_height = 500
 screen_width = 500
@@ -10,8 +13,11 @@ ball_speed=5
 ufo_width = 32
 ufo_height = 18 
 ufo_speed = 1
+spawn_speed = 0
 list_of_ufos = []
 frame_count = 0
+list_of_colors = [(239,7,73),(224,93,244),(93,216,244),(93,244,133),(222,244,93),(242,114,29)]
+color_id = 0
 # pygame.draw.circle(screen, (0,0,255),(250,250),75)
 screen = pygame.display.set_mode([screen_height, screen_width])
 pygame.display.set_caption('Alien_invasion')
@@ -55,8 +61,9 @@ while running:
     if space_pressed:
         number_of_empty_balls += 1 
         number_of_empty_balls=  number_of_empty_balls % max_number_of_empty_balls
-        if number_of_empty_balls == 0:
-            cannonball = [shipx+ship_width/2,shipy]
+        if number_of_empty_balls == 0:                                                      #number_of_empty_balls limits the amount of balls released by holding space
+            cannonball = [shipx+ship_width/2,shipy,list_of_colors[color_id]]
+            color_id = (color_id +1) % len(list_of_colors)
             list_of_cannonballs.append(cannonball)
 
     screen.fill((255,255,255))
@@ -73,18 +80,23 @@ while running:
         ball[1] -= ball_speed
         if ball[1] <0:
             del list_of_cannonballs[index]
-        pygame.draw.circle(screen, (255,0,0),tuple(ball),ball_radius)
+        pygame.draw.circle(screen, ball[2],tuple(ball[:2]),ball_radius)
 
         screen.blit(ball_image,(ball[0]-ball_radius,ball[1]-ball_radius))
+        correction = 0                                                  #because delete shortens the list->may cause index out of range
+
         for index,ufo in enumerate(list_of_ufos[:]):
             if ufo[1] + ufo_height >= ball[1] - ball_radius:
                 if ufo[0]>ball[0]-ball_radius and  ufo[0] < ball[0]+ball_radius or ufo[0]<ball[0] -ball_radius and ufo[0] + ufo_width >= ball[0]-ball_radius:
-                    del list_of_ufos[index]
+                    del list_of_ufos[index-correction]
+                    correction +=1
                 
 
-    if frame_count % 100 == 0:
-       current_ufo = [random.randint(ufo_width/2,screen_width-ufo_width),0]
-       list_of_ufos.append(current_ufo)
+    if frame_count % int(100-spawn_speed)== 0:
+        spawn_speed += 5 - (5*spawn_speed/99)
+        print (spawn_speed)
+        current_ufo = [random.randint(ufo_width/2,screen_width-ufo_width),0]
+        list_of_ufos.append(current_ufo)
 
     for index,ufo in enumerate(list_of_ufos[:]):
         ufo[1] += ufo_speed
@@ -99,5 +111,6 @@ while running:
     clock = pygame.time.Clock()
     clock.tick(60)
     frame_count +=1
+    ufo_speed += 0.001
  
 pygame.quit()
