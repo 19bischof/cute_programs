4cbc1e5bea01354e6aabd01e8a50fec4c5573060
Moritz Bischof <M.Bischof@it-technik.de>
2021-08-18 15:06:27 +0200

    Added Quicksort; changed the Sort-algorithm from Generator to appending progress to Lists; added boundaries in blue to the animation;cleaned up code

--- Viper/Sort/Animate.py
+++ Viper/Sort/Animate.py
@@ -1,12 +1,13 @@
+#Controller of UI of Window-Class,List-Class and Animates progress
 from settings import Settings as st
 import pygame 
 from window import  Window
 from list import List
 class Animate:
-    def render(list,vip=[]):
+    def render(list,vip=[],vvip=[]):
         Window.instance.screen.fill((0,0,0))
         width = int(st.screen_width / (len(list))) -1
-        height = (st.screen_height / (st.range)) 
+        height = int(st.screen_height / (st.range)) 
         if len(list)/st.screen_width > 0.50:
             print("list is too long, go for something like",int(st.screen_width*0.5))
             pygame.quit()
@@ -14,41 +15,90 @@ class Animate:
             color = (204, 151, 6)
             if index in vip:
                 color = (255,0,0)
-            pygame.draw.rect(Window.instance.screen,color,((1+width) * index,int(st.screen_height - bar * height)+1,width,int(bar*height)))
+            if index in vvip:
+                color = (39, 26, 232)
+            pygame.draw.rect(Window.instance.screen,color,((1+width) * index,(st.screen_height - bar * height)+1,width,bar*height))
         Window.update()
     def start():    
+        print(chr(27)+ "[2J")
         selection = ""    
-        while selection not in ["1","1)","Bubblesort","Bubble-sort","Bubble_sort","2","2)","Insertion-sort","Insertionsort","Insertion_sort","3","3)","Shakersort","Shaker_sort","Shaker-sort"]:
-            print("What sorting algorithm do you want to see?")
+        Bubblesort_set = ["1","1)","bubblesort","bubble-sort","bubble_sort"]
+        Insertionsort_set = ["2","2)","insertion-sort","insertionsort","insertion_sort"]
+        Shakersort_set = ["3","3)","shakersort","shaker_sort","shaker-sort"]
+        Quicksort_set = ["4","4)","quicksort","quick_sort","quick-sort"]
+        while selection not in Bubblesort_set + Insertionsort_set + Shakersort_set + Quicksort_set:
+            print("What Sorting-Algorithm do you want to see?")
             print("1) Bubblesort")
             print("2) Insertionsort")
             print("3) Shakersort")
-            selection = input()
+            print("4) Quicksort")
+            selection = input().lower()
         print("Press Space to start and to pause!")
-        if selection in ["1","1)","Bubblesort","Bubble-sort","Bubble_sort"]:
+        if selection in Bubblesort_set:
             Animate.Bubblesort()
-        if selection in ["2","2)","Insertion-sort","Insertionsort","Insertion_sort"]:
+        if selection in Insertionsort_set:
             Animate.Insertionsort()
-        if selection in ["3","3)","Shakersort","Shaker_sort","Shaker-sort"]:
+        if selection in Shakersort_set:
             Animate.Shakersort()
+        if selection in Quicksort_set:
+            Animate.Quicksort()
     def Bubblesort():
         Window("Bubblesort")
-        for l,i in List().Bubblesort_step_by_step():
+        _list = List()
+        _list.Bubblesort_step_by_step()
+        delay = int(1000 / len(_list.progress) + 7)
+        for p in _list.progress:         
             Window.check_events()
-            Animate.render(l,vip=[i])
-            pygame.time.delay(int(200/st.length))
+            Animate.render(p[0],vip=p[1],vvip=p[2])
+            pygame.time.delay(delay)
+        already_checked = []
+        for p in _list.check_progress:
+            Animate.check_loop(p,already_checked)
     def Insertionsort():
         Window("Insertionsort")
-        for l,i in List().Insertionsort_step_by_step():
+        _list = List()
+        _list.Insertionsort_step_by_step()
+        delay = int(5000 / len(_list.progress)+5)
+        for p in _list.progress: 
             Window.check_events()
-            Animate.render(l,vip=[i])
-            pygame.time.delay(int(5000/st.length))
+            Animate.render(p[0],vip=p[1],vvip=p[2])
+            pygame.time.delay(delay)
+        already_checked = []
+        for p in _list.check_progress:
+            Animate.check_loop(p,already_checked)
+
     def Shakersort():
         Window("Shakersort")
-        for l,i in List().Shakersort_step_by_step():
+        _list = List()
+        _list.Shakersort_step_by_step()
+        delay = int(1000 / len(_list.progress)+7)
+        for p in _list.progress:
             Window.check_events()
-            Animate.render(l,vip=[i])
-            pygame.time.delay(int(100*2/st.length))
+            Animate.render(p[0],vip=p[1],vvip=p[2])
+            pygame.time.delay(delay)
+        already_checked = []
+        for p in _list.check_progress:
+            Animate.check_loop(p,already_checked)
+    
+    def Quicksort():
+        Window("Quicksort")
+        _list = List()
+        _list.Quicksort_step_by_step()
+        delay = int(8000/len(_list.progress)+10)
+        for p in _list.progress:
+            Window.check_events()
+            Animate.render(p[0],vip=p[1],vvip=p[2])
+            pygame.time.delay(delay)
+        already_checked = []                                                    
+        for p in _list.check_progress:
+            Animate.check_loop(p,already_checked)
+
+    def check_loop(p,already_checked):
+        already_checked += p[1]                                         #inplace change
+        Window.check_events()
+        Animate.render(p[0],vvip=already_checked)
+        pygame.time.delay(int(1000/len(p[0])))
+
 Animate.start()
 pygame.quit()
 print("Done")
--- Viper/Sort/list.py
+++ Viper/Sort/list.py
@@ -1,62 +1,92 @@
+#When instance created sort-algorithm can be performed on it
+#stores step by step changes in progress-variable
+#check_sorted() progress is stored seperately to have the same speed on it in the animation later
 import random
 from settings import Settings as st
 from window import Window
 class List:
     def __init__(self):
+        self.values = []
         self.generate()
+        self.progress = []
+        self.check_progress = []
         
     def generate(self):
-        self.values = []
         for i in range(st.length):
             self.values.append(random.randint(0,st.range))
-            
+
     def Bubblesort_step_by_step(self):
         l = self.values
-        k = 0
-        for k in range(len(l)):
-            
+        for k in range(len(l)):            
             for i in range(len(l)-k-1):
                 if l[i]>l[i+1]:
                     l[i],l[i+1] = l[i+1],l[i]
-                    yield l,i+1
-                    
+                    self.progress.append((l[:],[i+1],[len(l)-k-1]))
         self.values = l
-        for ii in List.check_sorted(l):
-            yield l,ii        
+        self.check_sorted()
+
     def Insertionsort_step_by_step(self):
         l = self.values
         for i in range (len(l)):
-            for k in range(i):
-                
+            for k in range(i):                
                 if l[k]>l[i]:
                     l.insert(k,l[i])
                     del l[i+1]
-                    yield l,k
+                    self.progress.append((l[:],[k],[i+1]))
                     break
-        for ii in List.check_sorted(l):
-            yield l,ii
+        self.check_sorted()
+
     def Shakersort_step_by_step(self):
         l = self.values
         iteration = 0
         while iteration <= len(l)/2:
-            for i in range (iteration,len(l)-1):
+            for i in range (iteration,len(l)-1-iteration):
                 if l[i]>l[i+1]:
                     l[i],l[i+1] = l[i+1],l[i]
-                    yield l,i+1
-            iteration += 1
-            for k in range (len(l)-iteration,0,-1):
+                    self.progress.append((l[:],[i+1],[len(l)-1-iteration,iteration]))
+            for k in range (len(l)-iteration-1,iteration,-1):
                 if l[k-1] > l[k]:
                     l[k-1],l[k] = l[k],l[k-1]
-                    yield l,k-1
-        for ii in List.check_sorted(l):
-            yield l,ii
-    def check_sorted(l):
+                    self.progress.append((l[:],[k-1],[len(l)-1-iteration,iteration]))
+            iteration += 1
+        self.check_sorted()
+
+    def Quicksort_step_by_step(self):
+        l = self.values
+        self.recursive_quicksort(0,len(l)-1)
+        self.check_sorted()
+
+    def recursive_quicksort(self,lower_bound = 0,upper_bound = 0):
+        l = self.values
+        low = lower_bound
+        up = upper_bound
+        if low >= up:
+            return 
+        pivot = l[random.randint(low, up)]
+        while low < up:
+            while l[low] < pivot:
+                low += 1
+            while l[up] > pivot:
+                up -= 1
+            if low <= up:
+                l[low],l[up] = l[up],l[low]
+                low += 1;up -= 1
+                self.progress.append((l[:],[up,low],[lower_bound,upper_bound]))
+        self.recursive_quicksort(lower_bound,up)
+        self.recursive_quicksort(low,upper_bound)
+            
+
+
+    def check_sorted(self):
+        l = self.values
         for i in range (len(l)-1):
             if l[i] > l[i+1] :
                 print("The Algorithm is faulty and made a mistake!")
                 Window.in_focus = False                 #Pauses the Animation
                 Window.check_events()                 
                 return False
-            yield i+1
+            self.check_progress.append((l,[i]))
+        self.check_progress.append((l,[len(l)-1]))        #last index otherwise not highlighted
         return True                                     #Return doesn't do anything but ending the function early
+
 
