d91463a3492e819983d757faf18e5bd49344b553
bischof <moritzalbbw2@gmail.com>
2021-08-18 18:32:44 +0200

    fixed issue with large range of values(height was 0)

--- Viper/Sort/Animate.py
+++ Viper/Sort/Animate.py
@@ -7,7 +7,8 @@ class Animate:
     def render(list,vip=[],vvip=[]):
         Window.instance.screen.fill((0,0,0))
         width = int(st.screen_width / (len(list))) -1
-        height = int(st.screen_height / (st.range)) 
+        height = st.screen_height / (st.range)
+        
         if len(list)/st.screen_width > 0.50:
             print("list is too long, go for something like",int(st.screen_width*0.5))
             pygame.quit()
@@ -17,7 +18,7 @@ class Animate:
                 color = (255,0,0)
             if index in vvip:
                 color = (39, 26, 232)
-            pygame.draw.rect(Window.instance.screen,color,((1+width) * index,(st.screen_height - bar * height)+1,width,bar*height))
+            pygame.draw.rect(Window.instance.screen,color,((1+width) * index,int(st.screen_height - bar * height)+1,width,int(bar*height)))
         Window.update()
     def start():    
         print(chr(27)+ "[2J")
--- Viper/Sort/settings.py
+++ Viper/Sort/settings.py
@@ -1,5 +1,5 @@
 class Settings:
-    screen_width = 1000
-    screen_height = 1000
-    length = 100                #good values are 100,500 for 1000 width
-    range = 1000
+    screen_width = 1920
+    screen_height = 1080
+    length = 960               #good values are 100,500 for 1000 width
+    range = 10000
