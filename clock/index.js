cf328915276e4286088c26f665b7e48d43adecc0
Moritz Bischof <M.Bischof@it-technik.de>
2021-12-01 14:42:22 +0100

    fixed issue with pausing on seconds, now correctly gets the time right from time api call and updates on exact seconds change

--- clock/index.js
+++ clock/index.js
@@ -1,108 +1,165 @@
-var canvas = document.getElementById("canvas");
 // canvas.height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 // canvas.width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 // canvas.height = window.innerHeight
 // canvas.width = window.innerWidth
-canvas.height = document.documentElement.clientHeight
-canvas.width = document.documentElement.clientWidth
-var ctx = canvas.getContext('2d');
-audio_ctrl = document.getElementById("ticktock")
-class Clock {
 
-    constructor(x, y, radius) {
-        this.x = x
-        this.y = y
-        this.radius = radius
-        this.seconds = new Clock_hands(this.radius * 0.9, 5, "red")
-        this.minutes = new Clock_hands(this.radius * 0.9, 10, "black")
-        this.hours = new Clock_hands(this.radius * 0.7, 20, "black")
-        this.Clock_hands = [this.seconds, this.minutes, this.hours]
-        this.face = new Image()
-        this.face.src = "clock.png"
-        this.date = new Date()
+class Clock {
+    constructor(x, y, radius, time_diff) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.time_diff = time_diff;
+        this.seconds = new Clock_hands(this.radius * 0.9, 5, "red");
+        this.minutes = new Clock_hands(this.radius * 0.9, 10, "black");
+        this.hours = new Clock_hands(this.radius * 0.7, 20, "black");
+        this.Clock_hands = [this.seconds, this.minutes, this.hours];
+        this.face = new Image();
+        this.face.src = "clock.png";
+        this.get_date = () => {
+            return new Date(new Date().getTime() + this.time_diff);
+        };
+        this.audio_el = document.getElementById("ticktock");
+        this.audio_file = "tick.mp3";
     }
 
     calc_angles() {
-
-        this.date = new Date()
-        this.seconds.angle = this.date.getSeconds() / 60 * 2 * Math.PI
-        this.minutes.angle = this.date.getMinutes() / 60 * 2 * Math.PI
-        this.hours.angle = this.date.getHours() / 12 * 2 * Math.PI + this.minutes.angle / 12 + this.seconds.angle /(60*12) //for continuous motion on hour hand
+        this.date = this.get_date();
+        this.seconds.angle = (this.date.getSeconds() / 60) * 2 * Math.PI;
+        this.minutes.angle = (this.date.getMinutes() / 60) * 2 * Math.PI;
+        this.hours.angle =
+            (this.date.getHours() / 12) * 2 * Math.PI +
+            this.minutes.angle / 12 +
+            this.seconds.angle / (60 * 12); //for continuous motion on hour hand
 
         // because hands are at 12:00 at 3:00
-        this.seconds.angle = this.seconds.angle - Math.PI / 2
-        this.minutes.angle = this.minutes.angle - Math.PI / 2
-        this.hours.angle = this.hours.angle - Math.PI / 2
+        this.seconds.angle = this.seconds.angle - Math.PI / 2;
+        this.minutes.angle = this.minutes.angle - Math.PI / 2;
+        this.hours.angle = this.hours.angle - Math.PI / 2;
     }
-    draw() {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-        ctx.drawImage(this.face, this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2)
-        ctx.beginPath()
-        ctx.arc(this.x, this.y, this.radius * 0.04, 0, 2 * Math.PI)
-        ctx.fill()
-        ctx.stroke()
-        this.Clock_hands.forEach(e => {
-            ctx.translate(this.x, this.y)
-            ctx.beginPath()
-            ctx.rotate(e.angle)
-            ctx.rect(0, 0 - e.width / 2, e.height, e.width)
-            ctx.fillStyle = e.color
-            ctx.fill()
-            ctx.rotate(2 * Math.PI - e.angle)
+    draw(ctx) {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.drawImage(
+            this.face,
+            this.x - this.radius,
+            this.y - this.radius,
+            this.radius * 2,
+            this.radius * 2
+        );
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius * 0.04, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
+        this.Clock_hands.forEach((e) => {
+            ctx.translate;
+            ctx.translate(this.x, this.y);
+            ctx.beginPath();
+            ctx.rotate(e.angle);
+            ctx.rect(0, 0 - e.width / 2, e.height, e.width);
+            ctx.fillStyle = e.color;
+            ctx.fill();
+            ctx.rotate(2 * Math.PI - e.angle);
 
-            ctx.translate(-this.x, -this.y)
-        })
+            ctx.translate(-this.x, -this.y);
+        });
     }
     audio() {
-        var audio_file = audio_ctrl.src.substr(audio_ctrl.src.lastIndexOf('/')+1)
-        console.log(audio_file)
-        if (audio_file == "tock.mp3") {
-            audio_file = "tick.mp3"
+        // console.log(this.audio_file);
+        if (this.audio_file == "tock.mp3") {
+            this.audio_file = "tick.mp3";
         } else {
-            audio_file = "tock.mp3"
+            this.audio_file = "tock.mp3";
         }
-        audio_ctrl.src = audio_file
-        audio_ctrl.play()
+        this.audio_el.src = this.audio_file;
+        this.audio_el.play();
     }
-
 }
 class Clock_hands {
     constructor(height, width, color) {
-        this.height = height
-        this.width = width
-        this.angle = 0
-        this.color = color
+        this.height = height;
+        this.width = width;
+        this.angle = 0;
+        this.color = color;
     }
-
 }
 class Renderer {
+    constructor(time_diff) {
+        this.time_diff = time_diff;
+        this.canvas = document.getElementById("canvas");
+        this.update_canvas_and_clock();
+        this.get_date = () => {
+            return new Date(new Date().getTime() + this.time_diff);
+        };
 
-    constructor(canvas, ctx, clock) {
+        this.previous_seconds = -1; //if current second != prev_seconds => update()
+        window.onresize = this.update_canvas_and_clock.bind(this);
+    }
+    event_loop() {
+        let d = this.get_date();
+        let cur_seconds = d.getSeconds();
+        if (cur_seconds != this.previous_seconds) {
+            this.previous_seconds = cur_seconds;
+            this.do_update();
+        }
 
-        this.canvas = canvas
-        this.ctx = ctx
-        this.clock = clock
-
-
-
+        setTimeout(this.event_loop.bind(this), 1000 - d.getMilliseconds());
     }
 
-    update() {
-        this.clock.audio()
-        this.clock.calc_angles()
-        this.clock.draw()
-
-
+    do_update() {
+        this.clock.audio();
+        this.clock.calc_angles();
+        this.clock.draw(this.ctx);
+    }
+    update_canvas_and_clock() {
+        this.canvas.height = document.documentElement.clientHeight;
+        this.canvas.width = document.documentElement.clientWidth;
+        this.ctx = this.canvas.getContext("2d");
+        let clock_radius = 0;
+        if (canvas.height > canvas.width) {
+            clock_radius = canvas.width / 2;
+        } else {
+            clock_radius = canvas.height / 2;
+        }
+        this.clock = new Clock(
+            this.ctx.canvas.width / 2,
+            this.ctx.canvas.height / 2,
+            clock_radius,
+            this.time_diff
+        );
     }
 }
-if (ctx.canvas.height > ctx.canvas.width) {
-    clock_radius = ctx.canvas.width / 2
-} else {
-    clock_radius = ctx.canvas.height / 2
+
+function online_time() {
+    let machine_time = new Date();
+    fetch("http://worldtimeapi.org/api/timezone/Europe/Berlin", { cache: 'no-store' })  //no-store,so the browser always fetches from server and not cache
+        .then((value) => {
+            return value.json();
+        })
+        .then((data) => {
+            let real_utc_datetime = new Date(data["utc_datetime"]);
+            console.log("fetched time:", real_utc_datetime.getSeconds())
+            console.log("local time:", machine_time.getSeconds())
+            let avg_ping = 30
+            let time_diff = -avg_ping + (real_utc_datetime - machine_time); //add time_diff to all timings
+            if (Math.abs(time_diff) < 30) {
+                time_diff = 0;
+            }
+            let pronoun = "behind";
+            if (time_diff < 0) {
+                pronoun = "ahead";
+            }
+            document.getElementById("local_time_diff").textContent =
+                "The local Time is " +
+                pronoun +
+                " by " +
+                Math.abs(time_diff / 1000)
+                    .toFixed(2)
+                    .toString() +
+                " seconds!";
+            return time_diff;
+        })
+        .then((time_diff) => {
+            var my_render = new Renderer(time_diff);
+            my_render.event_loop();
+        });
 }
-var clock = new Clock(ctx.canvas.width / 2, ctx.canvas.height / 2, clock_radius)
-var renderer = new Renderer(canvas, ctx, clock);
-
-
-
-setInterval(renderer.update, 1000)
+online_time();
