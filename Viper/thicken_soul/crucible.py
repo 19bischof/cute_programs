1de1fd17d26d2cb4eeab2a96b72f186ebf97a9b3
Moritz <home@gmail.com>
2022-02-13 15:48:45 +0100

    added better optimizations and new massive crucible loop for more fun

---
+++ Viper/thicken_soul/crucible.py
@@ -0,0 +1,33 @@
+import thai
+from tqdm import tqdm
+
+# the following are simply time optimizations
+# there are a lot more optimizations that could be done
+thai.cons = False
+thai.no_early_finish_check = True
+thai.log_it = False
+thai.logging.getLogger().setLevel(thai.logging.WARNING)
+
+def one_game(methods):
+    i = thai.p_index
+    fin = False
+    while not fin:
+        methods[i]()
+        i = (i + 1) % len(methods)
+        fin = thai.finished()
+    if fin.find("1") != -1:
+        return 0
+    if fin.find("2") != -1:
+        return 1
+    return -1
+
+
+methods = {0: thai.bot4_ashley, 1: thai.bot3_anasthasia}
+results = []
+iterations = 3000
+for i in tqdm(range(iterations)):
+    results.append(one_game(methods))
+    thai.reset_game()
+print("0:    {:05.2f}%".format(100 * results.count(0) / iterations))
+print("1:    {:05.2f}%".format(100 * results.count(1) / iterations))
+print("draw: {:05.2f}%".format(100 * results.count(-1) / iterations))
--- Viper/thicken_soul/thai.py
+++ Viper/thicken_soul/thai.py
@@ -3,6 +3,7 @@
 # if two users put it 2 times
 # there are 5 bots that work in a hierarchy
 # with bot5 calling bot4 if it can't solve it and so on 
+# bot has to return when making a move!!
 import os
 from colored import fg, attr
 import random
@@ -14,7 +15,7 @@ logging.basicConfig(
     encoding="utf-8",
     format="%(asctime)s:%(levelname)s\t%(message)s",
     filemode="a",
-    level=logging.DEBUG,
+    level=logging.DEBUG
 )
 y_names = ("top", "center", "bottom")
 x_names = ("left", "middle", "right")
@@ -26,17 +27,18 @@ p_index = random.randrange(2)  # 0 is player1, 1 is player2
 p_colors = (fg("chartreuse_3a"), fg("red_3b"))
 pre = (p_colors[0] + "Player1", p_colors[1] + "Player2")
 clean = False #if already cleared console
-
-
+cons = True #if print to console
+no_early_finish_check = False #skip check if game still winnable (time_consuming)
+log_it = True #enable logging 
 def input_place_sign():
     # user method for placing sign
     global p_index
     stopping = False
-    logging.info("Player{} is choosing with input".format(p_index + 1))
+    if log_it: logging.info("Player{} is choosing with input".format(p_index + 1))
     while not stopping:
         os.system("cls" if os.name == "nt" else "clear")
         try:
-            logging.info("getting input from user")
+            if log_it: logging.info("getting input from user")
             inp = (
                 input(pre[p_index] + ", Where do you want to set it?\n").lower().strip()
             )
@@ -45,26 +47,26 @@ def input_place_sign():
             print_grid()
             quit()
         if inp in ("?", "help"):
-            logging.debug("calling help function")
+            if log_it: logging.debug("calling help function")
             print_help()
             input("Press Enter to continue!")
             continue
-        logging.debug("raw input is " + inp)
+        if log_it: logging.debug("raw input is " + inp)
         for x_pos in x_names:
             if inp.find(x_pos) != -1:
-                logging.debug("found x position in input: " + x_pos)
+                if log_it: logging.debug("found x position in input: " + x_pos)
                 for y_pos in y_names:
                     if inp.find(y_pos) != -1:
-                        logging.debug("found y position in input: " + y_pos)
+                        if log_it: logging.debug("found y position in input: " + y_pos)
                         if grid[y_names.index(y_pos)][x_names.index(x_pos)]:
-                            logging.debug(
+                            if log_it: logging.debug(
                                 "chosen position already taken "
                                 + repr((y_names.index(y_pos), x_names.index(x_pos)))
                             )
                             continue
                         grid[y_names.index(y_pos)][x_names.index(x_pos)] = p_index + 1
                         history.append((y_names.index(y_pos), x_names.index(x_pos)))
-                        logging.info(
+                        if log_it: logging.info(
                             "input set sign at ->{}<-".format(
                                 repr((y_names.index(y_pos), x_names.index(x_pos)))
                             )
@@ -75,32 +77,36 @@ def input_place_sign():
 
 def finished():
     # check if game finished
-    logging.debug("check for winner")
-    print(attr("reset"), end="", flush=True)
+    if log_it: logging.debug("check for winner")
+    if cons:print(attr("reset"), end="", flush=True)
     # check for winner
     for i in range(3):
         if grid[i][0] == grid[i][1] == grid[i][2] and grid[i][0]:
-            print(pre[grid[i][0] - 1] + " won the game!" + attr("reset"))
-            logging.info("Player{} won".format(grid[i][0]))
-            return True
+            if cons:print(pre[grid[i][0] - 1] + " won the game!" + attr("reset"))
+            if log_it: logging.info("Player{} won".format(grid[i][0]))
+            return "Player{} won".format(grid[i][0])
         if grid[0][i] == grid[1][i] == grid[2][i] and grid[0][i]:
-            print(pre[grid[0][i] - 1] + " won the game!" + attr("reset"))
-            logging.info("Player{} won".format(grid[0][i]))
-            return True
+            if cons:print(pre[grid[0][i] - 1] + " won the game!" + attr("reset"))
+            if log_it: logging.info("Player{} won".format(grid[0][i]))
+            return "Player{} won".format(grid[0][i])
     if grid[0][0] == grid[1][1] == grid[2][2] and grid[0][0]:
-        print(pre[grid[0][0] - 1] + " won the game!" + attr("reset"))
-        logging.info("Player{} won".format(grid[0][0]))
-        return True
+        if cons:print(pre[grid[0][0] - 1] + " won the game!" + attr("reset"))
+        if log_it: logging.info("Player{} won".format(grid[0][0]))
+        return "Player{} won".format(grid[0][0])
     if grid[2][0] == grid[1][1] == grid[0][2] and grid[2][0]:
-        print(pre[grid[2][0] - 1] + " won the game!" + attr("reset"))
-        logging.info("Player{} won".format(grid[2][0]))
-        return True
-    logging.debug("check for possibility to win")
+        if cons:print(pre[grid[2][0] - 1] + " won the game!" + attr("reset"))
+        if log_it: logging.info("Player{} won".format(grid[2][0]))
+        return "Player{} won".format(grid[2][0])
+    if log_it: logging.debug("check for possibility to win")
     # check if there can be a winner: return False if someone can win else return True
     # example: if line not full and line has not one of each -> someone can win
     # furthermore: if there are only 2 or 3 empty spaces in grid and those are located in a line
     # -> that line can never be won by one player! however someone can still win in another line somewhere else
     one_row_grid = grid[0] + grid[1] + grid[2]
+    if no_early_finish_check:
+        if one_row_grid.count(0) == 0:
+            return "no one wins"
+        return False
     trans = list(map(list, zip(*grid)))
     for i in range(3):
         if grid[i].count(0) != 0:
@@ -123,9 +129,9 @@ def finished():
                     pass
                 else:
                     return False
-    print("No one wins!")
-    logging.info("no possibility to win")
-    return True
+    if cons:print("No one wins!")
+    if log_it: logging.info("no possibility to win")
+    return "no one wins"
 
 
 def clear_screen():
@@ -142,20 +148,20 @@ def clear_screen():
 def func_place_sign(c: tuple[int]):  # c[0] is y; c[1] is x
     # bot method for placing sign
     global p_index
-    clear_screen()
+    if cons:clear_screen()
     if grid[c[0]][c[1]]:
         logging.error("Coordinates {} already taken".format(c))
         raise ValueError
     grid[c[0]][c[1]] = p_index + 1
     history.append((c[0], c[1]))
-    print(pre[p_index] + " sets it at {} {}".format(y_names[c[0]], x_names[c[1]]))
-    logging.info("function set sign at ->{}<-".format(repr(c)))
+    if cons:print(pre[p_index] + " sets it at {} {}".format(y_names[c[0]], x_names[c[1]]))
+    if log_it: logging.info("function set sign at ->{}<-".format(repr(c)))
     p_index = (p_index + 1) % 2
 
 
-def bot1_angela():
+def bot0_angela():
     # function: make random move
-    logging.info("Player{} is choosing with bot1_angela".format(p_index + 1))
+    if log_it: logging.info("Player{} is choosing with bot0_angela".format(p_index + 1))
     while True:
         c = (random.randrange(3), random.randrange(3))
         if not grid[c[0]][c[1]]:
@@ -163,9 +169,9 @@ def bot1_angela():
     func_place_sign(c)
 
 
-def bot2_amelie():
+def bot1_amelie():
     # function: make mirrored move of enemy's previous
-    logging.info("Player{} is choosing with bot2_amelie".format(p_index + 1))
+    if log_it: logging.info("Player{} is choosing with bot1_amelie".format(p_index + 1))
     calc = {0: 2, 1: 0, 2: -2}
     try:
         y, x = history[-1]
@@ -174,18 +180,18 @@ def bot2_amelie():
             if not grid[mir_y][mir_x]:
                 func_place_sign((mir_y, mir_x))
                 return
-            logging.debug("spot already taken " + repr((mir_y, mir_x)))
+            if log_it: logging.debug("spot already taken " + repr((mir_y, mir_x)))
     except IndexError:
         pass
-    logging.debug("unable to find a move")
-    bot1_angela()
+    if log_it: logging.debug("unable to find a move")
+    bot0_angela()
 
 
-def bot3_aurora():
+def bot2_aurora():
     # function: make move in corners and middle to form multiple lines at once
     # best corner is calculated by adding the score of row, column and diagonal
     # when no best corner: choose random corner
-    logging.info("Player{} is choosing with bot3_aurora".format(p_index + 1))
+    if log_it: logging.info("Player{} is choosing with bot2_aurora".format(p_index + 1))
     corners = ((0, 0), (2, 0), (2, 2), (0, 2))
     if not grid[1][1]:
         func_place_sign((1, 1))
@@ -216,7 +222,7 @@ def bot3_aurora():
                 line_score = -1
             corner_score += line_score
         if corner_score > best_corner_score:
-            logging.debug("corner_score updated to {}".format(corner_score))
+            if log_it: logging.debug("corner_score updated to {}".format(corner_score))
             best_corner = c
             best_corner_score = corner_score
     if best_corner:
@@ -228,20 +234,20 @@ def bot3_aurora():
         if not grid[y][x]:
             func_place_sign((y, x))
             return
-    logging.debug("unable to find a move")
-    bot2_amelie()
+    if log_it: logging.debug("unable to find a move")
+    bot1_amelie()
 
 
-def bot4_anasthasia():
+def bot3_anasthasia():
     # function: when one move from winning or losing make that one move
-    logging.info("Player{} is choosing with bot4_anasthasia".format(p_index + 1))
+    if log_it: logging.info("Player{} is choosing with bot3_anasthasia".format(p_index + 1))
     transp_grid = list(map(list, zip(*grid)))
     diags_index = (((0, 0), (1, 1), (2, 2)), ((2, 0), (1, 1), (0, 2)))
     # see if you are one away from winning
     for g in (grid, transp_grid):
         for y in range(3):
             if g[y].count(p_index + 1) == 2 and g[y].count(0) == 1:
-                logging.debug("found winning move!")
+                if log_it: logging.debug("found winning move!")
                 for x in range(3):
                     if not g[y][x]:
                         if g == transp_grid:
@@ -252,7 +258,7 @@ def bot4_anasthasia():
     for d in diags_index:
         line = [grid[y][x] for y, x in d]
         if line.count(p_index + 1) == 2 and line.count(0) == 1:
-            logging.debug("found winning move!")
+            if log_it: logging.debug("found winning move!")
             for y, x in d:
                 if not grid[y][x]:
                     func_place_sign((y, x))
@@ -263,7 +269,7 @@ def bot4_anasthasia():
     for g in (grid, transp_grid):
         for y in range(3):
             if g[y].count(next_p_index + 1) == 2 and g[y].count(0) == 1:
-                logging.debug("found enemy's winning move!")
+                if log_it: logging.debug("found enemy's winning move!")
                 for x in range(3):
                     if not g[y][x]:
                         if g == transp_grid:
@@ -274,21 +280,21 @@ def bot4_anasthasia():
     for d in diags_index:
         line = [grid[y][x] for y, x in d]
         if line.count(next_p_index + 1) == 2 and line.count(0) == 1:
-            logging.debug("found enemy's winning move!")
+            if log_it: logging.debug("found enemy's winning move!")
             for y, x in d:
                 if not grid[y][x]:
                     func_place_sign((y, x))
                     return
-    logging.debug("unable to find a move")
-    bot3_aurora()
+    if log_it: logging.debug("unable to find a move")
+    bot2_aurora()
 
 
-def bot5_ashley():
+def bot4_ashley():
     # function: move on pattern
     # pattern1: if enemy started with corner -> place in middle
     # pattern2: if enemy started with corner, you with middle, enemy with opposite corner
     # -> place in random edge
-    logging.info("Player{} is choosing with bot5_ashley".format(p_index + 1))
+    if log_it: logging.info("Player{} is choosing with bot4_ashley".format(p_index + 1))
     corners = ((0, 0), (2, 0), (2, 2), (0, 2))
     edges = ((0, 1), (1, 0), (2, 1), (1, 2))
     for c in corners:
@@ -300,8 +306,14 @@ def bot5_ashley():
         if history == [c, (1, 1), corners[i - 2]]:
             func_place_sign(random.choice(edges))
             return
-    logging.debug("unable to find a move")
-    bot4_anasthasia()
+    if history == []:
+        func_place_sign(random.choice(corners))
+        return
+    if len(history)==2 and history[0] in corners and history[1] == (1,1):
+        func_place_sign(corners[corners.index(history[0])-2])
+        return
+    if log_it: logging.debug("unable to find a move")
+    bot3_anasthasia()
 
 
 def print_grid():
@@ -337,22 +349,27 @@ def print_help():
             left middle right"""
     print(attr("reset") + h)
 
-
+def reset_game():
+    if log_it: logging.info("#################### resetting game ##################################")
+    global grid,history,p_index
+    grid = [[0] * 3 for x in range(3)]  # 0:empty, 1:player1, 2:player2
+    # syntax: grid[y][x]
+    history = []
+    p_index = random.randrange(2)
+    
 if __name__ == "__main__":
-    logging.info("Running in __main__")
+    if log_it: logging.info("Running in __main__")
     player_methods = {
-        0: bot4_anasthasia,
-        1: bot5_ashley
-        # 2: bot3_aurora,
-        # 3: bot1_angela,
+        0: bot3_anasthasia,
+        1: bot4_ashley
     }  # even is player1, odd is player2
-    logging.debug("player methods are " + repr(player_methods))
-    logging.debug("starting Game Loop")
+    if log_it: logging.debug("player methods are " + repr(player_methods))
+    if log_it: logging.debug("starting Game Loop")
     i = p_index
     while 1:
         player_methods[i]()
         if finished():
-            logging.debug("Break out of Game Loop")
+            if log_it: logging.debug("Break out of Game Loop")
             break
         if input_place_sign not in player_methods.values():
             print_grid()
