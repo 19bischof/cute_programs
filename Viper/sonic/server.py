5ba07ce17fe6073961ae6b877c7d5ade53abac66
Moritz <home@gmail.com>
2022-03-26 21:49:03 +0100

    each connection has its own socket now

--- Viper/sonic/server.py
+++ Viper/sonic/server.py
@@ -2,21 +2,22 @@ import logging
 import socket
 import time
 import config as cg
-import threading
+from _thread import start_new_thread
 logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(message)s")
 
 tees = []
 
-def send_dgrams(sock, client_address):
-    data = bytes([0xFF for _ in range(cg.chunk_size)])
-    start_t = time.perf_counter()
-    while time.perf_counter() - start_t < cg.duration:
-        try:
-            sock.sendto(data,client_address)
-        except ConnectionResetError:
-            logging.error(f"Connection with {client_address} was closed")
-            return
-    logging.info(f"finished sending to {client_address}...")
+def send_dgrams( client_address):
+    with socket.socket(type=socket.SOCK_DGRAM) as sock:
+        data = bytes([0xFF for _ in range(cg.chunk_size)])
+        start_t = time.perf_counter()
+        while time.perf_counter() - start_t < cg.duration:
+            try:
+                sock.sendto(data,client_address)
+            except ConnectionResetError:
+                logging.error(f"Connection with {client_address} was closed")
+                return
+        logging.info(f"finished sending to {client_address}...")
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
@@ -29,7 +30,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         except TimeoutError:
             continue
         logging.info(f"sending to {client_address}...")
-        t = threading.Thread(target=send_dgrams,args=(sock,client_address))
-        t.start()
-        tees.append(t)
+        start_new_thread(target=send_dgrams,args=[client_address])
+
         
