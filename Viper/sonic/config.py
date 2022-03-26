768ec3d94fbb312c51103b1f77e104c365202972
bischof <moritzalbbw2@gmail.com>
2022-03-26 13:17:49 +0100

    added instructions on address.json

--- Viper/sonic/config.py
+++ Viper/sonic/config.py
@@ -1,8 +1,18 @@
+import json
 chunk_size = 1024 * 8
 duration = 10
 timeout = 1
-import json
-with open("address.json","r") as f: #implement address.json locally
+
+with open("address.json", "r") as f:  # implement address.json locally
     dick = json.load(f)
-server_address = (dick["server_address"],dick["server_port"])
-client_address = (dick["client_address"],dick["client_port"])
+server_address = (dick["server_address"], dick["server_port"])
+client_address = (dick["client_address"], dick["client_port"])
+
+# local machine has to implement address.json
+# like so
+# {
+#     "server_address": "192.168.n.n",
+#     "server_port": 5556,
+#     "client_address": "192.168.n.n",
+#     "client_port" : 5557
+# }
