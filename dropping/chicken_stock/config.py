29b05fa455f9f653bc272948b27612debcbc7e00
bischof <moritzalbbw2@gmail.com>
2022-08-23 19:40:38 +0200

    deleted unncessary files and small cleanup

--- dropping/chicken_stock/config.py
+++ dropping/chicken_stock/config.py
@@ -1,7 +1,5 @@
 icoin_url = "http://127.0.0.1:5500/improv_exchange.html"
 tw_url = "https://twitch.tv/dashducks"
 tw_url = "https://twitch.tv/sodapoppin"
-fire_bin = r"C:\Users\m.bischof\AppData\Local\Mozilla Firefox\firefox.exe"
 fire_bin = None
-profile = "/home/moritz/.mozilla/firefox/jf8wa94q.testicle"
 headless = False
--- vial/tts/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup,Extension
-
-module = Extension("tts_mom",sources=["tts.c"])
-
-setup(name="dong",
-version="1.0",
-description="big dong",
-ext_modules = [module])
