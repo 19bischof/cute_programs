5808ed7ae665d434ad2203eecdc53aef6dfcef78
bischof <moritzalbbw2@gmail.com>
2022-08-23 15:33:45 +0200

    cleanup on fish

--- Viper/fish/fish.py
+++ Viper/fish/fish.py
@@ -1,13 +1,15 @@
 import csv
 from i_menu import i_menu
-from colored import fg,attr
+from colored import fg, attr
+import pathlib
+project_path = pathlib.Path(__file__).absolute().parent.as_posix()
 red = fg('red')
 reset = attr('reset')
 rows = []
 try:
-    csv_file_handle = open("fish.csv", encoding="utf-8")
+    csv_file_handle = open(project_path+"/fish.csv", encoding="utf-8")
 except FileNotFoundError:
-    print(red+"ERROR: "+ reset + "No 'fish.csv' file in directory!")
+    print(red+"ERROR: " + reset + "No 'fish.csv' file in directory!")
     quit()
 with csv_file_handle:
     csv_reader = csv.reader(csv_file_handle, delimiter=',')
@@ -19,12 +21,12 @@ with csv_file_handle:
         else:
             for cell in row:
                 if not cell:
-                    cell = "" #still False but now is a string and type uniform
+                    cell = ""  # still False but now is a string and type uniform
             rows.append(row)
             col_i += 1
 for i in range(len(rows)):
     rows[i].pop(0)
-column_heads.pop(0)#because first column is time of entry (useless)
+column_heads.pop(0)  # because first column is time of entry (useless)
 column_values = []
 for col_i in range(len(rows[0])):
     column_values.append([])
@@ -57,18 +59,18 @@ def get_distribution(chosen):
     for (real, possible) in zip(chosen, column_values):
         if real:
             assert real in possible
-    lod = [] #list of dicts. Each dict is on page or one column with each key a option and 
-    #the value the number of rows that match chosen pattern and have selected that option
+    lod = []  # list of dicts. Each dict is on page or one column with each key a option and
+    # the value the number of rows that match chosen pattern and have selected that option
 
     for column_i in range(len(column_heads)):
         dick = {key: 0 for key in column_values[column_i]}
         for row in rows:
-            able = True # a row matches the chosen pattern in this column
+            able = True  # a row matches the chosen pattern in this column
             for i in range(len(row)):
                 if chosen[i] is None:
                     continue
                 if row[i] != chosen[i]:
-                    able = False #somewhere the row doesn't match the chosen pattern anymore
+                    able = False  # somewhere the row doesn't match the chosen pattern anymore
             if not able:
                 continue
             dick[row[column_i]] += 1
@@ -80,12 +82,14 @@ def get_distribution(chosen):
     return lod
 
 
-res = [None for _ in range(len(rows))] #what has been selected (the chosen pattern)
+# what has been selected (the chosen pattern)
+res = [None for _ in range(len(rows))]
 m = i_menu(options=column_values, header=column_heads)
 value = len(rows)
 while True:
     m.result = f"Search results: {value} ({value/len(rows)*100:.1f}%)"
-    lod = get_distribution(res) #list of dicts, one dict per page, one key for each option
-    m.distros = lod #unnecessary but readable?
-    res = m.loop()#user input loop until user presses enter to choose option
-    value = filter(res) # count the rows matching new pattern
+    # list of dicts, one dict per page, one key for each option
+    lod = get_distribution(res)
+    m.distros = lod  # unnecessary but readable?
+    res = m.loop()  # user input loop until user presses enter to choose option
+    value = filter(res)  # count the rows matching new pattern
